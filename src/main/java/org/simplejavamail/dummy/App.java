fdea1bf31b351b6eb54c4a792f62ef697d6a4902
bbottema <b.bottema@projectnibble.org>
2019-02-14 15:10:37 +0100

    initial commit

---
+++ src/main/java/org/simplejavamail/dummy/App.java
@@ -0,0 +1,13 @@
+package org.simplejavamail.dummy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
